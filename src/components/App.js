4650f7f20a93bb28557babaf7c4617295f241422
Andrew Balanuk <abalanuk@lohika.com>
2017-02-07 17:11:23 +0200

    Added common component: Navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 // import { Link, IndexLink } from 'react-router';
+import Header from './common/Header';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -9,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div class="container-fluid">
-        <header>Header here...</header>
+        <Header/>
         {this.props.children}
       </div>
     );
--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
   return (
-    <div>
+    <div className="main">
       <h2 className="alt-header">About</h2>
       <p>
         This App uses React, Redux, React Router and other useful libs
