4372f46595b426c7d76789919a472c41c439c986
Andrew Balanuk <abalanuk@lohika.com>
2017-02-21 17:35:09 +0200

    Thunk installed, added mock API, connected thunk with store + getting list of courses

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,50 +6,19 @@ import * as courseActions from '../../actions/courseActions'
 class CoursesPage extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			course: {title: ""}
-		};
-
-		//bind section
-		this.onClickSave = this.onClickSave.bind(this)
-		this.onTitleChange = this.onTitleChange.bind(this)
-	}
-
-	onTitleChange(event) {
-		const course = this.state.course
-		course.title = event.target.value
-		this.setState({course: course})
-	}
-
-	onClickSave() {
-		// alert(`Saving ${this.state.course.title}`)
-		this.props.actions.createCourse(this.state.course)
 	}
 
 	_courseRow(course, index) {
-		return <div key={index}>{course.title}</div>
+		return <li key={index}>{course.title}</li>
 	}
 
 	render() {
-		debugger
 		return (
 	      <div className="coursesList">
 	        <h2>Courses</h2>
-	        {this.props.courses.map(this._courseRow)}
-	        <h3>Add Course</h3>
-	        <div className="form-group">
-		        <input
-					type='text'
-					value={this.state.course.title}
-					onChange={this.onTitleChange}
-					className="form-control" />
-
-		        <input
-					type='submit'
-					value="Save"
-					onClick={this.onClickSave}
-					className="btn btn-danger" />
-			</div>
+	        <ol>
+	        	{this.props.courses.map(this._courseRow)}
+	        </ol>
 	      </div>
 		);
 	}
@@ -57,19 +26,16 @@ class CoursesPage extends React.Component {
 
 
 CoursesPage.defaultProps = {
-	actions: () => {},
-	courses: [{title: 'Front-End'}]
+	courses: [{title: 'Something went wrong with Data'}]
 }
 
 CoursesPage.propTypes = {
-	actions: PropTypes.object.isRequired,
 	courses: PropTypes.array.isRequired
 }
 
 
 //state -> represents state inside our store
 function mapStateToProps(state, ownProps){
-	debugger
 	return {
 		//key courses -> will be call inside our component: this.props.courses
 		courses: state.courses //state.courses it's what we define as key inside rootReducer
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 /* eslint-disable import/default */
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
 //Attaches redux store to our container components
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
 /* Local import */
-import configureStore from './store/configureStore';
-import routes from './routes';
+import configureStore from './store/configureStore'
+import {loadCourses} from './actions/courseActions'
+import routes from './routes'
 
 // You can import SASS/CSS files too!
 //Webpack will run the associated loader and plug this into the page.
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-const store = configureStore();
+const store = configureStore()
+
+store.dispatch(loadCourses())
 
 ReactDOM.render(
 	<Provider store={store}>
